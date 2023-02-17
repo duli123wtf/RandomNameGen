69524b09d2d4684be0c7ec34a9f6d699650adf64
Alexis <dulantoariasalexis50@gmail.com>
2023-02-17 09:00:26 -0500

    finalidk

--- public/scripts/index.js
+++ public/scripts/index.js
@@ -1,36 +1,31 @@
-import { surnames, names } from "./constants.js";
-
-const generate = () => {
-    const updateFullName = (gender) => {
-        const quantityNames = Math.floor(Math.random() * 2) + 1;
-        const specifyNames = names[gender];
-        let fullName = "";
-        for (let i = 0; i < quantityNames; i++) {
-            fullName += specifyNames[Math.floor(Math.random() * specifyNames.length)] + " ";
-        }
-        fullName += surnames[Math.floor(Math.random() * surnames.length)] + " ";
-        fullName += surnames[Math.floor(Math.random() * surnames.length)];
-        fullName = fullName.trim();
-        document.querySelector('#name-aleatory h2').innerText = fullName;
-    };
-
-    const btnMale = document.querySelector('#btn-man');
-    btnMale.addEventListener('click', () => updateFullName('male'));
-
-    const btnFemale = document.querySelector('#btn-woman');
-    btnFemale.addEventListener('click', () => updateFullName('female'));
-
-    const btnGeneral = document.querySelector('#btn-general');
-    btnGeneral.addEventListener('click', () => {
-        let gender;
-        if (Math.random() < 0.5) {
-            gender = 'male';
-        } else {
-            gender = 'female';
-        }
-        updateFullName(gender);
-    });
-
-
+import { surnames, names, GENDERS } from './constants.js';
+const getRandomNumber = (max = 11) => {
+    return Math.floor(Math.random() * max);
 };
-generate();
+const generateFullName = (gender) => {
+    const quantityNames = getRandomNumber(2) + 1;
+    const specifyNames = names[gender];
+    let fullName = '';
+    for (let i = 0; i < quantityNames; i++) {
+        fullName += specifyNames[getRandomNumber(specifyNames.length)] + ' ';
+    }
+    fullName += surnames[getRandomNumber(surnames.length)] + ' ';
+    fullName += surnames[getRandomNumber(surnames.length)];
+    fullName = fullName.trim();
+    //Llenando nombre aleatorio
+    document.querySelector('#name-aleatory h2').innerText = fullName;
+};
+const updateGeneralFullName = () => {
+    const gender = Math.random() < 0.5 ? GENDERS.MALE : GENDERS.FEMALE;
+    generateFullName(gender);
+};
+const initialize = () => {
+    // Inicializando botones
+    document.querySelector('#btn-man')
+        .addEventListener('click', () => generateFullName(GENDERS.MALE));
+    document.querySelector('#btn-woman')
+        .addEventListener('click', () => generateFullName(GENDERS.FEMALE));
+    document.querySelector('#btn-general')
+        .addEventListener('click', updateGeneralFullName);
+};
+initialize();
